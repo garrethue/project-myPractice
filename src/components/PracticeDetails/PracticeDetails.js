75b97a4d8dc8f8b917686d4aa8ac48ac52514e37
Garret Larson <garret.hs.larson@gmail.com>
2020-10-27 11:48:40 -0500

    Bare minimum styling. Practice details table is rendering.

--- src/components/PracticeDetails/PracticeDetails.js
+++ src/components/PracticeDetails/PracticeDetails.js
@@ -8,11 +8,24 @@ function PracticeDetails(props) {
   console.log(props.store);
   return (
     <div>
-      <Button>Start</Button>
-      <br />
-      <Button>Edit</Button>
-      <br />
-      <Button>Delete</Button>
+      <Grid
+        bg="blue"
+        margin={5}
+        justifyContent="center"
+        alignItems="center"
+        templateColumns="repeat(2, 1fr)"
+        gap={4}
+      >
+        {props.store.practiceDetails.map((poseObj) => {
+          return (
+            <>
+              <Box>{poseObj.pose_name}</Box>
+              <Box>{poseObj.pose_time}</Box>
+            </>
+          );
+        })}
+      </Grid>
+      <Button>Start</Button> <Button>Edit</Button> <Button>Delete</Button>
     </div>
   );
 }
